243106f6d8eee14afe5c68428e4d9bbf82ed838a
Yuyuhita Guzman <yuyuhita.guzman@gmail.com>
2021-07-26 22:47:05 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(){//returnFirstTwoDrivers() anonymous
+    const group = ["Sally", "Bob", "Freddy", "Claudia"]
+    return group.slice(0, 2)
+}
+
+function returnLastTwoDrivers(){
+    const group2 = ["Sally", "Bob", "Freddy", "Claudia"]
+    return group2.slice(-2, 4)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier= (integer)=>{
+    return integer=> (integer*5)
+}
+
+function fareDoubler(price){
+    const double=createFareMultiplier(price)(4)
+    return double
+}
+
+function fareTripler(price){
+    const triple=createFareMultiplier(price)(7.2)
+    return triple
+}
+
+const selectDifferentDrivers=(drivers, returnFirstTwoDrivers)=>returnFirstTwoDrivers(drivers.slice(0,2))
